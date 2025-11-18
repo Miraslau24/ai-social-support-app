f6fa6ca978c20a2ff1587aa22cbb7b718b344122
miraslau.r <user@users-Mac-mini-2.local>
2025-11-18 14:48:08 +0100

    feature: the server has been added

---
+++ server/src/server.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import config from './config';
+
+const app = express();
+
+app.listen(config.PORT, () => {
+  console.log(`Server running: http://localhost:${config.PORT}`);
+});
