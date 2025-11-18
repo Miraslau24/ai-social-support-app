4cba276305aea62bd3673421ef57f7ab638ec47b
miraslau.r <user@users-Mac-mini-2.local>
2025-11-18 13:49:28 +0100

    fix: move front to client folder

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
@@ -9,11 +8,9 @@ import { persistor, store } from './store/store.ts';
 import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')!).render(
-  //<StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
-  //</StrictMode>
 );
